0a358b19cf47f5681b78cadb1d806fd68ec89c7a
SolveigD <solveigdelamarre@yahoo.fr>
2024-01-07 16:52:10 +0100

    modification fonction resetmodale

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -4,7 +4,9 @@ const bandeauNoir = document.querySelector('.bandeau');
 const mesProjets = document.querySelector('.mes_projets');
 const gallery = document.querySelector(".gallery");
 const zoneEdition = document.querySelector('.zone_edition');
-const user = window.localStorage.getItem('user');
+const logOut = document.querySelector('.logout_accueil');
+let user = window.localStorage.getItem('user');
+let zoneFiltre = null;
 let newImage = null;
 let categories = null;
 let travaux = null;
@@ -20,103 +22,114 @@ let image = null;
 const editButton = document.getElementById('zoneEdit');
 const closeButton = document.querySelector('.close');
 const overlay = document.querySelector('.overlay');
-const changerPage2 = document.querySelector('.changer_page_2');
-const changerPage1 = document.querySelector('.back');
+const changePage2 = document.querySelector('.changer_page_2');
+const changePage1 = document.querySelector('.back');
 const envoyerImage = document.querySelector('.btn_envoyer_img');
 const validerForm = document.querySelector('#validerForm');
+const formulaireTravaux = document.querySelector('.form_ajout_travaux');
+const inputImage = document.querySelector('#inputImage');
 
 editButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-changerPage2.addEventListener('click', function() {
-    changerPage(2);
+changePage2.addEventListener('click', function() {
+    changePage(2);
 });
-changerPage1.addEventListener('click', function() {
-    changerPage(1);
+changePage1.addEventListener('click', function() {
+    changePage(1);
 });
 
 envoyerImage.addEventListener('click', function (e){
     capturerImage();   
 });
 
-validerForm.addEventListener('click', validerFormulaire);
+formulaireTravaux.addEventListener('input', updaterBoutonValider);
+
+validerForm.addEventListener('click', validateFormulaire);
 
 // Fin Js pour le modal
 
 async function init() {
-    await getTravaux();
-    afficherTravaux();
-    await afficherFiltres();
-    afficherTravauxModal();
-    afficherCategoriesOption();
+    await getWorks();
+    showWorks();
+    await showFiltres();
+    showWorksModal();
+    showCategoryOptions();
+    checkLoginStatus();
 }
 
-async function getTravaux() {
+async function getWorks() {
     const reponse = await fetch("http://localhost:5678/api/works/");
     travaux = await reponse.json();
 }
 
-function afficherTravaux() {
+function showWorks() { 
     let i = 0
     for (i = 0; i < travaux.length; i++) {
-       
-        const figure = document.createElement("figure");
-        figure.setAttribute('class', 'work');
-        figure.setAttribute('data-category-id', travaux[i].categoryId);
-        figure.setAttribute('data-work-id', travaux[i].id);
-
-        const imageTravaux = document.createElement("img");
-        imageTravaux.src = travaux[i].imageUrl;
-  
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerHTML = travaux[i].title;
-        gallery.appendChild(figure);
-        figure.appendChild(imageTravaux);
-        figure.appendChild(figcaption);
-
+        addWork(travaux[i].id, travaux[i].categoryId, travaux[i].imageUrl, travaux[i].title);
     }
 }
 
-function afficherTravauxModal() {
-    const gallery = document.querySelector('.modalGallery');
-    
+function showWorksModal() {
     for (let i = 0; i < travaux.length; i++) {
-        const containerImage = document.createElement('div');
-        containerImage.classList.add('container_image_modal');
-        containerImage.setAttribute('data-work-id', travaux[i].id);
-
-        const imageModal = document.createElement('img');
-        imageModal.src = travaux[i].imageUrl;
-        imageModal.classList.add('image_modal');
-
-        const trashCan = document.createElement('img');
-        trashCan.src = "./assets/icons/trash-can-solid.svg";
-        trashCan.classList.add('trashcan')
-
-        gallery.append(containerImage);
-        containerImage.appendChild(imageModal)
-        containerImage.appendChild(trashCan);
-        
-        trashCan.addEventListener('click', async function(event) {
-            const container = event.target.parentElement;
-            const id = Number(container.getAttribute('data-work-id'));
-            const reponse = await supprimerTravailAPI(id);
-
-            if (reponse) {
-                //Deleter le travail dans le modal
-                container.remove();
-
-                // Deleter le travail sur la page
-                const  travail = document.querySelector(`[data-work-id="${id}"]`);
-                travail.remove();
-            } else {
-                alert('Erreur!');
-            }
-        })
+        addWorkModal(travaux[i].id, travaux[i].imageUrl);
     }
 }
 
-async function supprimerTravailAPI(id) {
+function addWork(id, categoryId, imageUrl, title) {
+    const figure = document.createElement("figure");
+    figure.setAttribute('class', 'work');
+    figure.setAttribute('data-category-id', categoryId);
+    figure.setAttribute('data-work-id', id);
+
+    const imageTravaux = document.createElement("img");
+    imageTravaux.src = imageUrl;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = title;
+    gallery.appendChild(figure);
+    figure.appendChild(imageTravaux);
+    figure.appendChild(figcaption);
+}
+
+function addWorkModal(id, imageUrl) {
+    const gallery = document.querySelector('.modalGallery');
+
+    const containerImage = document.createElement('div');
+    containerImage.classList.add('container_image_modal');
+    containerImage.setAttribute('data-work-id', id);
+
+    const imageModal = document.createElement('img');
+    imageModal.src = imageUrl;
+    imageModal.classList.add('image_modal');
+
+    const trashCan = document.createElement('img');
+    trashCan.src = "./assets/icons/trash-can-solid.svg";
+    trashCan.classList.add('trashcan')
+
+    gallery.append(containerImage);
+    containerImage.appendChild(imageModal)
+    containerImage.appendChild(trashCan);
+    
+    trashCan.addEventListener('click', async function(event) {
+        const container = event.target.parentElement;
+        const id = Number(container.getAttribute('data-work-id'));
+        const reponse = await deleteWorkInApi(id);
+
+        if (reponse) {
+            //Deleter le travail dans le modal
+            container.remove();
+
+            // Deleter le travail sur la page
+            const  travail = document.querySelector(`[data-work-id="${id}"]`);
+            travail.remove();
+        } else {
+            alert('Erreur!');
+        }
+    })
+}
+
+async function deleteWorkInApi(id) {
     const user = JSON.parse(window.localStorage.getItem('user'));
     const response = await fetch("http://localhost:5678/api/works/" + id, {
       method: "DELETE",
@@ -130,12 +143,12 @@ async function supprimerTravailAPI(id) {
     return response.ok;
 }
 
-async function afficherFiltres(){
+async function showFiltres(){
     const reponse = await fetch("http://localhost:5678/api/categories/");
     categories = await reponse.json();    
     
     // Ajout elements dans HTML
-    const zoneFiltre = document.createElement("div");
+    zoneFiltre = document.createElement("div");
     zoneFiltre.className = 'zone_filtre';
     const emplacementZoneFiltre = document.querySelector('#portfolio h2');
     emplacementZoneFiltre.appendChild(zoneFiltre);
@@ -163,17 +176,20 @@ async function afficherFiltres(){
         })
     }
 
+}
+
+function checkLoginStatus(){
     if (user) {
         loginAccueil.classList.add('inactive');
         logoutAccueil.classList.remove('inactive');
         bandeauNoir.classList.remove('inactive');
         zoneFiltre.classList.add('inactive');
         mesProjets.classList.add('margin_bottom');
+        zoneEdition.classList.remove('inactive');
     } else {
         logoutAccueil.classList.add('inactive');
         zoneEdition.classList.add('inactive');
     }
-    
 }
 
 function filtreTravaux(id) {
@@ -193,11 +209,11 @@ function filtreTravaux(id) {
     }
 }
 
-function logout() {
+logOut.addEventListener('click', function(){
     window.localStorage.clear();
     window.location.replace('./login.html');
-}
-
+})
+    
 function openModal() {
     const modale = document.getElementById('modale1');
     modale.classList.remove('hidden');
@@ -208,17 +224,17 @@ function closeModal() {
     modale.classList.add('hidden');
 }
 
-function changerPage(numeroDePage) {
+function changePage(pageNumber) {
     const modalPage1 = document.querySelector('.modalPage1');
     const modalPage2 = document.querySelector('.modalPage2');
     const arrowBack = document.querySelector('.back');
 
-    if (numeroDePage === 1) {
+    if (pageNumber === 1) {
         // afficher page 1 et cacher page 2
         modalPage1.classList.remove('hidden');
         modalPage2.classList.add('hidden');
         arrowBack.classList.add('hide');
-    } else if (numeroDePage === 2) {
+    } else if (pageNumber === 2) {
         // afficher page 2 et cacher page 1
         modalPage1.classList.add('hidden');
         modalPage2.classList.remove('hidden');
@@ -226,7 +242,7 @@ function changerPage(numeroDePage) {
     }
 }
 
-async function afficherCategoriesOption (){
+async function showCategoryOptions (){
     const selectCategories = document.querySelector(".select_category");
 
     for (let i = 0; i < categories.length; i++){
@@ -240,100 +256,92 @@ async function afficherCategoriesOption (){
 
 }
 
-async function capturerImage(){
+async function capturerImage() {
     const telechargerImage = document.querySelector('.telecharger_img');
     await telechargerImage.click();
 
     telechargerImage.addEventListener('change', function loaderImage() {
-        image = telechargerImage.files[0];   
+        image = telechargerImage.files[0];
         telechargerImage.removeEventListener('change', loaderImage);
-
-        if (image) {
+        const tailleEnMo = 4;
+        const tailleEnOctets = tailleEnMo * 1024 * 1024;
+        if (image && image.size < tailleEnOctets ) {
             const pasImage = document.querySelector('.pasImage');
             const ouiImage = document.querySelector('.ouiImage');
             pasImage.classList.add('hidden');
             ouiImage.classList.remove('hidden');
+            updaterBoutonValider();
 
             const imageContainer = document.querySelector('.ouiImage img');
             const blob = new Blob([image], { type: image.type });
             newImage =  URL.createObjectURL(blob);
             imageContainer.src = newImage;
             formData.append('image', image);
-
-           
+        } else {
+            alert('image trop grande');
         }
     });
-
 }
 
+async function validateFormulaire() {
+    const titreImage = document.querySelector('.label_titre');
+    const titreImageValue = titreImage.value;
+    const selectCategories = document.querySelector('.select_category');
+    const selectedOption = selectCategories.selectedOptions[0];
+    const categoryId = selectedOption.id;
+        
+    formData.append('title', titreImageValue);
+    formData.append('category', Number(categoryId));
+    
+    user = JSON.parse(window.localStorage.getItem('user'));
 
-async function validerFormulaire() {
-     
+    const reponse = await fetch('http://localhost:5678/api/works/', {
+        method: 'POST',
+        headers: { 
+        
+            "Authorization": `Bearer ${user.token}`
+        },
+        body: formData
+    });
 
+    if (reponse.ok) {
+        const newWork = await reponse.json();
+        
+        addWork(newWork.id, newWork.categoryId, newWork.imageUrl, newWork.title);
+        addWorkModal(newWork.id, newWork.imageUrl);
+        
+        resetModal();
+    }
+}
+
+function resetModal() {
+    formData = new FormData();
+    image = null;
+    formulaireTravaux.reset();
+
+    const pasImage = document.querySelector('.pasImage');
+    const ouiImage = document.querySelector('.ouiImage');
+    pasImage.classList.remove('hidden');
+    ouiImage.classList.add('hidden');
+
+    updaterBoutonValider();
+    changePage(1);
+    closeModal();
+}
+
+function updaterBoutonValider() {
     const titreImage = document.querySelector('.label_titre');
     const titreImageValue = titreImage.value;
     const selectCategories = document.querySelector('.select_category');
     const selectedOption = selectCategories.selectedOptions[0];
     const categoryId = selectedOption.id;
     
-  
-        
-        formData.append('title', titreImageValue);
-        formData.append('category', Number(categoryId));
-        
-        const user = JSON.parse(window.localStorage.getItem('user'));
-
-        const answer = await fetch('http://localhost:5678/api/works/', {
-            method: 'POST',
-            headers: { 
-            
-                "Authorization": `Bearer ${user.token}`
-            },
-            body: formData
-        });
-
-      
-
-       if (answer.ok) {
-      //    addNewWork();
-      const figure = document.createElement('figure');    
-      const image = document.createElement('img');
-      image.src = newImage;
-      const titre = document.createElement('figcaption');
-      titre.innerHTML = titreImageValue;
-      figure.classList.add('work');
-      figure.setAttribute('data-category-id', categoryId);
-    
-
-      gallery.appendChild(figure);
-      figure.appendChild(image);
-      figure.appendChild(titre);
-      
-      closeModal();
-          
-       }
-        
+    const isFormValid = titreImageValue != "" && categoryId != "" && image;
+    if (isFormValid) {
+        validerForm.disabled = false;
+    } else {
+        validerForm.disabled = true;
+    }
 }
-
-function addNewWork() {
-  
-  //  const figure = document.createElement('figure');
-   // const image = document.createElement('img');
-    
- //   figure.setAttribute('data-category-id', Number(categoryId));
- 
-   
-  //  image.src = newImage;
-  // const titre = document.createElement('figcaption');
- //  titre.innerHTML = titreImageValue;
-   
-  // gallery.appendChild(figure);
-  // figure.appendChild(image);
-  // figure.appendChild(titre);
-
-  
-
-}  
-
 
 init();
